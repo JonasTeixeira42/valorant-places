e3630223c586719d448e42d025d189849d76444c
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2021-06-29 21:32:06 -0300

    adicionado tema no css

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,3 @@
-import * as S from './styles';
-
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um atomo e React Avançado escrito ao lado"
-    />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJs, NextJS, Styled-components
-    </S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Desenvolvedor de frente para uma tela com código"
-    />
-  </S.Wrapper>
-);
+const Main = () => <a href="#">Street Fighter Places</a>;
 
 export default Main;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado</title>
+        <title>Street Fighters</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show every place a street fighter's character came from"
         />
       </Head>
       <GlobalStyles />
