34574023cb35faf5caf7654efae8dc45ef687763
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2021-07-12 20:56:19 -0300

    gerado paths das urls dinamicas

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,29 @@
-import { GET_PAGES } from 'graphql/queries';
+import { useRouter } from 'next/dist/client/router';
 
 import client from 'graphql/client';
+import { GET_PAGES } from 'graphql/queries';
 
 import PageTemplate from 'templates/Pages';
 
 export default function AboutPage() {
+  const router = useRouter();
+
+  if (router.isFallback) return null;
+
   return <PageTemplate />;
 }
 
-export const getStaticProps = async () => {
+export async function getStaticPaths() {
   const { pages } = await client.request(GET_PAGES);
 
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }));
+
+  return { paths, fallback: true };
+}
+
+export const getStaticProps = async () => {
   return {
     props: {}
   };
