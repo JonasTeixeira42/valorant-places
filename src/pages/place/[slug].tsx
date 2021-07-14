0eef51c9dda98b862460d03115e55e79594a662c
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2021-07-13 21:38:28 -0300

    criado template de places

---
+++ src/pages/place/[slug].tsx
@@ -0,0 +1,49 @@
+import client from 'graphql/client';
+import { useRouter } from 'next/dist/client/router';
+import { GetStaticProps } from 'next';
+
+import PlaceTemplate, { PlaceTemplateProps } from 'templates/Places';
+
+import { GET_PLACES, GET_PLACE_BY_SLUG } from 'graphql/queries';
+
+import {
+  GetPlacesBySlugQuery,
+  GetPlacesQuery
+} from 'graphql/generated/graphql';
+
+export default function Page({ place }: PlaceTemplateProps) {
+  const router = useRouter();
+
+  if (router.isFallback) return null;
+
+  return <PlaceTemplate place={place} />;
+}
+
+export async function getStaticPaths() {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, {
+    first: 3
+  });
+
+  const paths = places.map(({ slug }) => ({
+    params: { slug }
+  }));
+
+  return { paths, fallback: true };
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { place } = await client.request<GetPlacesBySlugQuery>(
+    GET_PLACE_BY_SLUG,
+    {
+      slug: `${params?.slug}`
+    }
+  );
+
+  if (!place) return { notFound: true };
+
+  return {
+    props: {
+      place
+    }
+  };
+};
---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,30 @@
+type ImageProps = {
+  url: string;
+  height: string;
+  width: string;
+};
+
+export type PlaceTemplateProps = {
+  place: {
+    slug: string;
+    name: string;
+    description: {
+      html: string;
+    };
+    gallery: ImageProps[];
+  };
+};
+
+export default function PlaceTemplate({ place }: PlaceTemplateProps) {
+  return (
+    <>
+      <h1>{place.name}</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+
+      {place.gallery.map((image, index) => (
+        <img key={index} src={image.url} alt={place.name} />
+      ))}
+    </>
+  );
+}
