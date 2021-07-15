01f6559d5d337d5e7a146e39089bab70c4c169dc
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2021-07-14 21:08:41 -0300

    feito estilos da pagina place

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,11 @@
+import { useRouter } from 'next/dist/client/router';
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
+import Image from 'next/image';
+
+import LinkWrapper from 'components/LinkWrapper';
+
+import * as S from './styles';
+
 type ImageProps = {
   url: string;
   height: string;
@@ -8,7 +16,7 @@ export type PlaceTemplateProps = {
   place: {
     slug: string;
     name: string;
-    description: {
+    description?: {
       html: string;
     };
     gallery: ImageProps[];
@@ -16,15 +24,40 @@ export type PlaceTemplateProps = {
 };
 
 export default function PlaceTemplate({ place }: PlaceTemplateProps) {
+  const router = useRouter();
+
+  if (router.isFallback) return null;
+
   return (
     <>
-      <h1>{place.name}</h1>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
 
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+          <S.Body
+            dangerouslySetInnerHTML={{
+              __html: place.description?.html || 'null'
+            }}
+          />
 
-      {place.gallery.map((image, index) => (
-        <img key={index} src={image.url} alt={place.name} />
-      ))}
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={index}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+                objectFit={'cover'}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   );
 }
